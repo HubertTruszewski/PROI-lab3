3eafc8e37dbaa6e125d81c08755b39ed0a966cc4
Hubert Truszewski <01150052@pw.edu.pl>
2021-04-01 14:55:07 +0200

    Disable procompiled headers and remove pch file

--- SetLib/SetLib.cpp
+++ SetLib/SetLib.cpp
@@ -1,10 +1,1 @@
-// SetLib.cpp : Defines the functions for the static library.
-//
-
-#include "pch.h"
-#include "framework.h"
-
-// TODO: This is an example of a library function
-void fnSetLib()
-{
-}
+#include "SetLib.h"
