c21bcd2d45f1a5ce5fcf83285603c63b9ae90fb7
Hubert Truszewski <01150052@pw.edu.pl>
2021-04-07 12:43:58 +0200

    Add declaration of Set class

--- SetLib/SetLib.cpp
+++ SetLib/SetLib.cpp
@@ -1,1 +1,75 @@
 #include "SetLib.h"
+
+Set::Set() noexcept
+{
+}
+
+std::vector<int> Set::getElements() const noexcept
+{
+	// TODO: insert return statement here
+}
+
+std::ostream& Set::operator<<(Set const& s) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator+(Set const& s) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator-(Set const& s) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator+=(Set const& s) noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator-=(Set const& s) noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator+(int a) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator-(int a) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator+=(int a) noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator-=(int a) noexcept
+{
+	// TODO: insert return statement here
+}
+
+Set Set::operator*(Set const& s) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+bool Set::isInSet(int a) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+bool Set::isEmpty(int a) const noexcept
+{
+	// TODO: insert return statement here
+}
+
+int Set::count() const noexcept
+{
+	// TODO: insert return statement here
+}
--- SetLib/SetLib.h
+++ SetLib/SetLib.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <iostream>
+#include <vector>
+
+class Set
+{
+private:
+	std::vector<int> container;
+
+public:
+	Set() noexcept;
+	std::vector<int> getElements() const noexcept;
+	std::ostream& operator<<(Set const& s) const noexcept;
+	Set operator+(Set const& s) const noexcept;
+	Set operator-(Set const& s) const noexcept;
+	Set operator+=(Set const& s) noexcept;
+	Set operator-=(Set const& s) noexcept;
+	Set operator+(int a) const noexcept;
+	Set operator-(int a) const noexcept;
+	Set operator+=(int a) noexcept;
+	Set operator-=(int a) noexcept;
+	Set operator*(Set const& s) const noexcept;
+	bool isInSet(int a) const noexcept;
+	bool isEmpty(int a) const noexcept;
+	int count() const noexcept;
+
+
+};
