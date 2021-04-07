91d22122c99fd04daf9bd5271e078ef3e49f0d1d
Hubert Truszewski <01150052@pw.edu.pl>
2021-04-07 18:37:48 +0200

    Add unit tests

--- SetApp/SetApp.cpp
+++ SetApp/SetApp.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
 #include "../SetLib/SetLib.h"
 
 int main()
 {
-    Set set;
-    set += 6;
-    set += 9;
-    std::cout << set;
+	Set set1;
+	set1 += 5;
+	set1 += 10;
+	Set set2;
+	set2 += 23;
+	set2 += 10;
+	Set set3 = set1 + set2;
+	std::cout << set1 << std::endl;
+	std::cout << set2 << std::endl;
+	std::cout << set3 << std::endl;
 }
 
--- SetLibTest/SetLibTest.cpp
+++ SetLibTest/SetLibTest.cpp
@@ -1,5 +1,7 @@
 #include "CppUnitTest.h"
 #include "../SetLib/SetLib.h"
+#include <iostream>
+#include <sstream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -13,7 +15,7 @@ namespace SetLibTest
 		TEST_METHOD(TestDefaultConstructor)
 		{
 			Set testSet;
-			Assert::AreEqual(testSet.count(), (size_t) 0);
+			Assert::AreEqual(testSet.count(), (size_t)0);
 			Assert::IsTrue(testSet.isEmpty());
 			Assert::IsFalse(testSet.isInSet(5));
 			Assert::IsFalse(testSet.isInSet(0));
@@ -22,7 +24,7 @@ namespace SetLibTest
 		{
 			std::vector<int> numbers = { 1, 2, 3, 4 };
 			Set testSet(numbers);
-			Assert::AreEqual(testSet.count(), (size_t) 4);
+			Assert::AreEqual(testSet.count(), (size_t)4);
 			Assert::IsTrue(testSet.isInSet(1));
 			Assert::IsTrue(testSet.isInSet(4));
 			Assert::IsFalse(testSet.isInSet(10));
@@ -32,7 +34,7 @@ namespace SetLibTest
 		{
 			std::vector<int> numbers = { 1, 2, 3, 4, 3, 6, 2, 5, 4, 9, 1 };
 			Set testSet(numbers);
-			Assert::AreEqual(testSet.count(), (size_t) 7);
+			Assert::AreEqual(testSet.count(), (size_t)7);
 			Assert::IsTrue(testSet.isInSet(1));
 			Assert::IsTrue(testSet.isInSet(4));
 			Assert::IsTrue(testSet.isInSet(9));
@@ -43,7 +45,7 @@ namespace SetLibTest
 		{
 			std::vector<int> numbers = {};
 			Set testSet(numbers);
-			Assert::AreEqual(testSet.count(), (size_t) 0);
+			Assert::AreEqual(testSet.count(), (size_t)0);
 			Assert::IsTrue(testSet.isEmpty());
 			Assert::IsFalse(testSet.isInSet(5));
 			Assert::IsFalse(testSet.isInSet(0));
@@ -52,7 +54,7 @@ namespace SetLibTest
 		{
 			Set emptySet;
 			Set testSet(emptySet);
-			Assert::AreEqual(testSet.count(), (size_t) 0);
+			Assert::AreEqual(testSet.count(), (size_t)0);
 			Assert::IsTrue(testSet.isEmpty());
 			Assert::IsFalse(testSet.isInSet(5));
 			Assert::IsFalse(testSet.isInSet(0));
@@ -62,7 +64,7 @@ namespace SetLibTest
 			std::vector<int> numbers = { 1, 2 , 3, 4 };
 			Set sourceSet(numbers);
 			Set testSet(sourceSet);
-			Assert::AreEqual(testSet.count(), (size_t) 4);
+			Assert::AreEqual(testSet.count(), (size_t)4);
 			Assert::IsTrue(testSet.isInSet(2));
 			Assert::IsTrue(testSet.isInSet(4));
 			Assert::IsFalse(testSet.isInSet(8));
@@ -73,7 +75,7 @@ namespace SetLibTest
 			std::vector<int> numbers = {};
 			Set testSet(numbers);
 			auto result = testSet.getElements();
-			Assert::AreEqual(result.size(), (size_t) 0);
+			Assert::AreEqual(result.size(), (size_t)0);
 			Assert::IsTrue(result.empty());
 		}
 		TEST_METHOD(TestGetElementsNormal)
@@ -81,7 +83,7 @@ namespace SetLibTest
 			std::vector<int> numbers = {1, 2, 3, 4, 5};
 			Set testSet(numbers);
 			auto result = testSet.getElements();
-			Assert::AreEqual(result.size(), (size_t) 5);
+			Assert::AreEqual(result.size(), (size_t)5);
 			Assert::IsFalse(result.empty());
 		}
 		TEST_METHOD(TestGetElementsNotUniqueNums)
@@ -89,7 +91,7 @@ namespace SetLibTest
 			std::vector<int> numbers = {2, 4, 7, 2, 4, 0, 6, 4, 7};
 			Set testSet(numbers);
 			auto result = testSet.getElements();
-			Assert::AreEqual(result.size(), (size_t) 5);
+			Assert::AreEqual(result.size(), (size_t)5);
 			Assert::IsFalse(result.empty());
 		}
 		TEST_METHOD(TestOperatorPlusWithSetUniqueNums)
@@ -99,7 +101,7 @@ namespace SetLibTest
 			std::vector<int> numbers2 = { 6, 7, 9 };
 			Set set2(numbers2);
 			Set set3 = set1 + set2;
-			Assert::AreEqual(set3.count(), (size_t) 8);
+			Assert::AreEqual(set3.count(), (size_t)8);
 			Assert::IsTrue(set3.isInSet(3));
 			Assert::IsTrue(set3.isInSet(7));
 			Assert::IsFalse(set3.isInSet(8));
@@ -112,12 +114,342 @@ namespace SetLibTest
 			std::vector<int> numbers2 = { 5, 6, 7, 9 };
 			Set set2(numbers2);
 			Set set3 = set1 + set2;
-			Assert::AreEqual(set3.count(), (size_t) 9);
+			Assert::AreEqual(set3.count(), (size_t)9);
 			Assert::IsTrue(set3.isInSet(3));
 			Assert::IsTrue(set3.isInSet(7));
 			Assert::IsTrue(set3.isInSet(8));
 			Assert::IsFalse(set3.isEmpty());
 			Assert::IsFalse(set3.isInSet(0));
+		}
+		TEST_METHOD(TestOperatorPlusWithSetEmpty)
+		{
+			std::vector<int> numbers = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers);
+			Set set2;
+			Set set3 = set1 + set2;
+			Assert::AreEqual(set3.count(), (size_t)7);
+			Assert::IsTrue(set3.isInSet(3));
+			Assert::IsFalse(set3.isInSet(7));
+			Assert::IsTrue(set3.isInSet(8));
+			Assert::IsFalse(set3.isEmpty());
+			Assert::IsFalse(set3.isInSet(0));
+		}
+		TEST_METHOD(TestOperatorMinusWithSetNormal)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 3, 4, 9 };
+			Set set2(numbers2);
+			Set set3 = set1 - set2;
+			Assert::AreEqual(set3.count(), (size_t)4);
+			Assert::IsTrue(set3.isInSet(5));
+			Assert::IsFalse(set3.isInSet(7));
+			Assert::IsTrue(set3.isInSet(8));
+			Assert::IsFalse(set3.isEmpty());
+			Assert::IsFalse(set3.isInSet(0));
+		}
+		TEST_METHOD(TestOperatorMinusWithSetNoCommon)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 10, 13 };
+			Set set2(numbers2);
+			Set set3 = set1 - set2;
+			Assert::AreEqual(set3.count(), (size_t)7);
+			Assert::IsFalse(set3.isEmpty());
+			Assert::IsTrue(set3.isInSet(9));
+			Assert::IsFalse(set3.isInSet(10));
+		}
+		TEST_METHOD(TestOperatorMinusWithEmptySet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			Set set2;
+			Set set3 = set1 - set2;
+			Assert::AreEqual(set3.count(), (size_t)7);
+			Assert::IsFalse(set3.isEmpty());
+			Assert::IsTrue(set3.isInSet(3));
+			Assert::IsTrue(set3.isInSet(9));
+			Assert::IsFalse(set3.isInSet(7));
+		}
+		TEST_METHOD(TestOperatorPlusWithInteger)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			Set set2 = set1 + 6;
+			Assert::AreEqual(set2.count(), (size_t)8);
+			Assert::IsFalse(set2.isEmpty());
+			Assert::IsTrue(set2.isInSet(5));
+			Assert::IsTrue(set2.isInSet(6));
+			Assert::IsFalse(set2.isInSet(7));
+		}
+		TEST_METHOD(TestOperatorPlusWithIntegerInSet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			Set set2 = set1 + 9;
+			Assert::AreEqual(set2.count(), (size_t)7);
+			Assert::IsFalse(set2.isEmpty());
+			Assert::IsTrue(set2.isInSet(5));
+			Assert::IsTrue(set2.isInSet(9));
+			Assert::IsFalse(set2.isInSet(7));
+		}
+		TEST_METHOD(TestOperatorMinusWithIntegerInSet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			Set set2 = set1 - 5;
+			Assert::AreEqual(set2.count(), (size_t)6);
+			Assert::IsFalse(set2.isEmpty());
+			Assert::IsTrue(set2.isInSet(8));
+			Assert::IsFalse(set2.isInSet(5));
+		}
+		TEST_METHOD(TestOperatorMinusWithIntegerNotInSet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			Set set2 = set1 - 6;
+			Assert::AreEqual(set2.count(), (size_t)7);
+			Assert::IsFalse(set2.isEmpty());
+			Assert::IsTrue(set2.isInSet(8));
+			Assert::IsFalse(set2.isInSet(6));
+		}
+		TEST_METHOD(TestOperatorAdditionAssignmentWithSetNormal)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 6, 7 };
+			Set set2(numbers2);
+			set1 += set2;
+			Assert::AreEqual(set1.count(), (size_t)9);
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(6));
+			Assert::IsFalse(set1.isEmpty());
+		}
+		TEST_METHOD(TestOperatorAdditionAssignmentWithSetCommonElements)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 1, 3, 5 };
+			Set set2(numbers2);
+			set1 += set2;
+			Assert::AreEqual(set1.count(), (size_t)7);
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+			Assert::IsFalse(set1.isEmpty());
+		}
+		TEST_METHOD(TestOperatorAdditionAssignmentWithSetNotUniqueElements)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 1, 3, 5, 6, 7 };
+			Set set2(numbers2);
+			set1 += set2;
+			Assert::AreEqual(set1.count(), (size_t)9);
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(5));
+			Assert::IsTrue(set1.isInSet(6));
+			Assert::IsFalse(set1.isEmpty());
+		}
+		TEST_METHOD(TestOperatorAdditionAssignmentWithEmptySet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			Set set2;
+			set1 += set2;
+			Assert::AreEqual(set1.count(), (size_t)7);
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+			Assert::IsFalse(set1.isEmpty());
+		}
+		TEST_METHOD(TestOperatorSubstractionAssignmentNormal)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 1, 3, 5 };
+			Set set2(numbers2);
+			set1 -= set2;
+			Assert::AreEqual(set1.count(), (size_t)4);
+			Assert::IsTrue(set1.isInSet(2));
+			Assert::IsTrue(set1.isInSet(9));
+			Assert::IsFalse(set1.isInSet(1));
+			Assert::IsFalse(set1.isInSet(3));
+			Assert::IsFalse(set1.isInSet(5));
+		}
+		TEST_METHOD(TestOperatorSubstractionAssignmentWithCommonElements)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 5, 8, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 1, 3, 5, 6, 7 };
+			Set set2(numbers2);
+			set1 -= set2;
+			Assert::AreEqual(set1.count(), (size_t)4);
+			Assert::IsTrue(set1.isInSet(2));
+			Assert::IsTrue(set1.isInSet(9));
+			Assert::IsFalse(set1.isInSet(1));
+			Assert::IsFalse(set1.isInSet(3));
+			Assert::IsFalse(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+			Assert::IsFalse(set1.isInSet(7));
+		}
+		TEST_METHOD(TestOperatorSubstractionAssignmentWithEmptySet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4 };
+			Set set1(numbers1);
+			Set set2;
+			set1 -= set2;
+			Assert::AreEqual(set1.count(), (size_t)4);
+			Assert::IsTrue(set1.isInSet(1));
+			Assert::IsTrue(set1.isInSet(2));
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(4));
+			Assert::IsFalse(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+			Assert::IsFalse(set1.isInSet(7));
+		}
+		TEST_METHOD(TestOperatorSubstractionAssignmentWithNotCommon)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 5, 7, 9 };
+			Set set2(numbers2);
+			set1 -= set2;
+			Assert::AreEqual(set1.count(), (size_t)4);
+			Assert::IsTrue(set1.isInSet(1));
+			Assert::IsTrue(set1.isInSet(2));
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(4));
+			Assert::IsFalse(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+			Assert::IsFalse(set1.isInSet(7));
+		}
+		TEST_METHOD(TestOperatorSubstractionAssignmentWithPresentIneger)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4 };
+			Set set1(numbers1);
+			set1 -= 4;
+			Assert::AreEqual(set1.count(), (size_t)3);
+			Assert::IsTrue(set1.isInSet(1));
+			Assert::IsTrue(set1.isInSet(2));
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsFalse(set1.isInSet(4));
+			Assert::IsFalse(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+		}
+		TEST_METHOD(TestOperatorSubstractionAssignmentWithNotPresentInteger)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4 };
+			Set set1(numbers1);
+			set1 -= 6;
+			Assert::AreEqual(set1.count(), (size_t)4);
+			Assert::IsTrue(set1.isInSet(1));
+			Assert::IsTrue(set1.isInSet(2));
+			Assert::IsTrue(set1.isInSet(3));
+			Assert::IsTrue(set1.isInSet(4));
+			Assert::IsFalse(set1.isInSet(5));
+			Assert::IsFalse(set1.isInSet(6));
+		}
+		TEST_METHOD(TestIntersectionNormal)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 7, 9 };
+			Set set1(numbers1);
+			std::vector<int> numbers2 = { 1, 5, 8, 9 };
+			Set set2(numbers2);
+			Set set3 = set1 * set2;
+			Assert::AreEqual(set3.count(), (size_t)2);
+			Assert::IsFalse(set3.isEmpty());
+			Assert::IsTrue(set3.isInSet(1));
+			Assert::IsTrue(set3.isInSet(9));
+			Assert::IsFalse(set3.isInSet(3));
+			Assert::IsFalse(set3.isInSet(8));
+		}
+		TEST_METHOD(TestIntersectionEmptySet)
+		{
+			std::vector<int> numbers1 = { 1, 2, 3, 4, 7, 9 };
+			Set set1(numbers1);
+			Set set2;
+			Set set3 = set1 * set2;
+			Assert::AreEqual(set3.count(), (size_t)0);
+			Assert::IsTrue(set3.isEmpty());
+			Assert::IsFalse(set3.isInSet(1));
+			Assert::IsFalse(set3.isInSet(9));
+			Assert::IsFalse(set3.isInSet(3));
+			Assert::IsFalse(set3.isInSet(8));
+		}
+		TEST_METHOD(TestIntersectionEmptySets)
+		{
+			Set set1;
+			Set set2;
+			Set set3 = set1 * set2;
+			Assert::AreEqual(set3.count(), (size_t)0);
+			Assert::IsTrue(set3.isEmpty());
+			Assert::IsFalse(set3.isInSet(1));
+			Assert::IsFalse(set3.isInSet(9));
+			Assert::IsFalse(set3.isInSet(3));
+			Assert::IsFalse(set3.isInSet(8));
+		}
+
+		TEST_METHOD(TestIsInSetTrue)
+		{
+			Set testSet;
+			testSet += 5;
+			testSet += 7;
+			Assert::IsTrue(testSet.isInSet(5));
+			Assert::IsTrue(testSet.isInSet(7));
+		}
+		TEST_METHOD(TestIsInSetFalse)
+		{
+			Set testSet;
+			testSet += 5;
+			testSet += 7;
+			Assert::IsFalse(testSet.isInSet(9));
+			Assert::IsFalse(testSet.isInSet(8));
+		}
+		TEST_METHOD(TestIsEmptyTrue)
+		{
+			Set testSet;
+			Assert::IsTrue(testSet.isEmpty());
+		}
+		TEST_METHOD(TestIsEmptyFalse)
+		{
+			Set testSet;
+			testSet += 9;
+			Assert::IsFalse(testSet.isEmpty());
+		}
+		TEST_METHOD(TestCountEmptySet)
+		{
+			Set set1;
+			Assert::AreEqual(set1.count(), (size_t)0);
+		}
+		TEST_METHOD(TestCountNotEmptySet)
+		{
+			Set set1;
+			set1 += 6;
+			set1 += 9;
+			Assert::AreEqual(set1.count(), (size_t)2);
+		}
+		TEST_METHOD(TestInsertionOperatorEmptySet)
+		{
+			std::stringstream out;
+			Set testSet;
+			out << testSet;
+			if (out.str() == "{}")
+			{
+				Assert::IsTrue(1);
+			}
+			else {
+				Assert::Fail;
+			}
+		}
+		TEST_METHOD(TestInsertionOperatorNotEmpty)
+		{
+			std::vector<int> numbers = { 1, 2, 19 };
+			Set set1(numbers);
+			std::stringstream out;
+			out << set1;
+			Assert::AreEqual(out.str().length(), (size_t)10);
 		}
 	};
 }
